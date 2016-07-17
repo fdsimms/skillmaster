61a0a6cf854a7beee0173194266b2578f21ff99a
Frankie <fdsimms@gmail.com>
2016-07-17 19:12:29 -0400

    feat: create SplashPage component

---
+++ ios/js/components/SplashPage/stylesheet.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+    padding: 10
+  },
+  logo: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  button: {
+    height: 50,
+    borderWidth: 1,
+    marginTop: 4,
+    backgroundColor: "#48BBEC",
+    padding: 4,
+    alignSelf: "stretch",
+    justifyContent: "center"
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#FFF",
+    alignSelf: "center"
+  }
+});
+
+export default styles;
