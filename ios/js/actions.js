b9e96ff125c6eab3251f2146226e5e4cc33df302
Frankie <fdsimms@gmail.com>
2016-07-24 17:00:04 -0400

    feat: action and reducer for ErrorHandler

--- ios/js/actions.js
+++ ios/js/actions.js
@@ -1,5 +1,7 @@
 export const HIDE_PROGRESS = "HIDE_PROGRESS";
 export const SHOW_PROGRESS = "SHOW_PROGRESS";
+export const UPDATE_ERROR_MESSAGE = "UPDATE_ERROR_MESSAGE";
+export const RESET_ERROR_MESSAGE = "RESET_ERROR_MESSAGE";
 
 export function showProgress() {
   return {
@@ -10,5 +12,18 @@ export function showProgress() {
 export function hideProgress() {
   return {
     type: HIDE_PROGRESS
+  };
+}
+
+export function updateErrorMessage(errorMessage) {
+  return {
+    type: UPDATE_ERROR_MESSAGE,
+    errorMessage
+  };
+}
+
+export function resetErrorMessage() {
+  return {
+    type: RESET_ERROR_MESSAGE
   };
 }
--- ios/js/reducers.js
+++ ios/js/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import {
   SHOW_PROGRESS,
-  HIDE_PROGRESS
+  HIDE_PROGRESS,
+  UPDATE_ERROR_MESSAGE,
+  RESET_ERROR_MESSAGE
 } from "./actions";
 
 function displayProgress(state = false, action) {
@@ -15,8 +17,20 @@ function displayProgress(state = false, action) {
   }
 }
 
+function errorMessage(state = "", action) {
+  switch (action.type) {
+    case UPDATE_ERROR_MESSAGE:
+      return action.errorMessage;
+    case RESET_ERROR_MESSAGE:
+      return "";
+    default:
+      return state;
+  }
+}
+
 const rootReducer = combineReducers({
-  displayProgress
+  displayProgress,
+  errorMessage
 });
 
 export default rootReducer;
