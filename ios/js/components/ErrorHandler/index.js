42f097134e23163d14a034e0545a6228cb991890
Frankie <fdsimms@gmail.com>
2016-07-24 17:00:38 -0400

    feat: very basic ErrorHandler component

---
+++ ios/js/components/ErrorHandler/index.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from "react";
+import { connect } from "react-redux";
+import { Text } from "react-native";
+
+import styles from "./stylesheet";
+
+const ErrorHandler = ({ message }) => (
+  message ? <Text style={styles.text}>{message}</Text> : null
+);
+
+function mapStateToProps(state) {
+  return { message: state.errorMessage };
+}
+
+ErrorHandler.propTypes = { message: PropTypes.string.isRequired };
+
+export default connect(mapStateToProps)(ErrorHandler);
