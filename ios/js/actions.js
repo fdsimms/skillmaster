1350360133ccdd339055895a8d57d728c9677923
Frankie <fdsimms@gmail.com>
2016-07-31 19:36:18 -0400

    feat: receiveCurrentUser action

--- ios/js/actions.js
+++ ios/js/actions.js
@@ -2,6 +2,7 @@ export const HIDE_SPINNER = "HIDE_SPINNER";
 export const SHOW_SPINNER = "SHOW_SPINNER";
 export const UPDATE_ERROR_MESSAGE = "UPDATE_ERROR_MESSAGE";
 export const RESET_ERROR_MESSAGE = "RESET_ERROR_MESSAGE";
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 
 export function showSpinner() {
   return {
@@ -25,5 +26,12 @@ export function updateErrorMessage(errorMessage) {
 export function resetErrorMessage() {
   return {
     type: RESET_ERROR_MESSAGE
+  };
+}
+
+export function receiveCurrentUser(currentUser) {
+  return {
+    type: RECEIVE_CURRENT_USER,
+    currentUser
   };
 }
