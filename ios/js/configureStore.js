049a7f3028ab149259e2fa4c32b2e426e6b5f670
Frankie <fdsimms@gmail.com>
2016-07-31 21:30:20 -0400

    feat: create navigatorIOSMiddleware

--- ios/js/configureStore.js
+++ ios/js/configureStore.js
@@ -1,13 +1,26 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import rootReducer from "./reducers";
+import { CHANGE_SCENE } from "./actions";
+
+function navigatorIOSMiddleware() {
+  return (next) => (action) => {
+    if (action.type === CHANGE_SCENE) {
+      const { title, component } = action.currentScene;
+
+      action.navigator.push({ title, component: component.type });
+    }
+    return next(action);
+  };
+}
 
 export default function configureStore(initialState) {
   return createStore(
     rootReducer,
     initialState,
     applyMiddleware(
-      thunkMiddleware
+      thunkMiddleware,
+      navigatorIOSMiddleware
     )
   );
 }
