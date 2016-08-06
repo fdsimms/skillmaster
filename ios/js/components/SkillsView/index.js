7f471446c0ec13c82ce0c6b69b096dce710c1697
Frankie <fdsimms@gmail.com>
2016-08-06 17:14:08 -0400

    feat: skeleton for skills view

---
+++ ios/js/components/SkillsView/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  Text,
+  View
+} from "react-native";
+import styles from "./stylesheet";
+
+
+const SkillsView = () => (
+  <View style={styles.container}>
+    <Text style={styles.logo}>Skills</Text>
+  </View>
+);
+
+export default SkillsView;
