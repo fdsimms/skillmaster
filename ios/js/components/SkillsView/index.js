48838655e73749e2cd5b973d8912927aff8d8666
Frankie <fdsimms@gmail.com>
2016-08-06 18:37:36 -0400

    feat: fetch skills in SkillsView

--- ios/js/components/SkillsView/index.js
+++ ios/js/components/SkillsView/index.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { PropTypes, Component } from "react";
+import { connect } from "react-redux";
+
 import {
   Text,
   View
 } from "react-native";
 import styles from "./stylesheet";
+import { fetchSkills } from "../../apiUtils";
 
+class SkillsView extends Component {
+  componentDidMount() {
+    if (!this.props.skills.length) {
+      this.props.dispatch(fetchSkills());
+    }
+  }
 
-const SkillsView = () => (
-  <View style={styles.container}>
-    <Text style={styles.logo}>Skills</Text>
-  </View>
+  get skills() {
+    const { skills } = this.props;
+    return skills.length > 0 && (
+      skills.map(skill => (
+        <Text key={skill.id} style={styles.logo}>{skill.name}</Text>
+      ))
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.logo}>Skills</Text>
+        <View style={styles.container}>
+          {this.skills}
+        </View>
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = (state) => (
+  { skills: state.skills }
 );
 
-export default SkillsView;
+SkillsView.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  navigator: PropTypes.object.isRequired,
+  skills: PropTypes.array.isRequired
+};
+
+export default connect(mapStateToProps)(SkillsView);
