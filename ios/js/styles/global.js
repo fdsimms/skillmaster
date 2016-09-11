56c8c7ddcb45b71d91c89428473785a2d8e8f6e3
Frankie <fdsimms@gmail.com>
2016-09-11 16:01:04 -0400

    Remove useless styles from global stylesheet

--- ios/js/styles/global.js
+++ ios/js/styles/global.js
@@ -1,46 +1,19 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({
+const fonts = StyleSheet.create({
+  lobsterRegular: {
+    fontFamily: "lobster-Regular"
+  }
+});
+
+const gs = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#1DACD6",
     padding: 10
   },
-  siteLogo: {
-    fontSize: 36,
-    color: "#FFFFFF",
-    textAlign: "center",
-    fontFamily: "Lobster-Regular",
-    margin: 2.5
-  },
-  button: {
-    height: 50,
-    marginTop: 10,
-    backgroundColor: "#FAC219",
-    padding: 4,
-    width: 250,
-    borderRadius: 5,
-    alignSelf: "center",
-    justifyContent: "center",
-    margin: 10
-  },
-  buttonText: {
-    fontSize: 20,
-    color: "#FFFFFF",
-    alignSelf: "center",
-    fontWeight: "bold"
-  },
-  basicText: {
-    fontSize: 16,
-    color: "#FFFFFF"
-  },
-  headerText: {
-    fontSize: 28,
-    color: "#FFFFFF",
-    fontFamily: "Lobster-Regular"
-  },
+  // TODO figure out why doing inputs with NativeTachyons breaks them
   input: {
     height: 50,
     marginTop: 2.5,
@@ -53,4 +26,4 @@ const styles = StyleSheet.create({
   }
 });
 
-export default styles;
+export { gs, fonts };
