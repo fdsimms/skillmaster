9e4d10474e5b844037e9659fae12c382ce7de2af
Frankie <fdsimms@gmail.com>
2016-09-11 14:45:45 -0400

    Initialize react-native-style-tachyons in entry point

--- index.ios.js
+++ index.ios.js
@@ -3,6 +3,20 @@ import { AppRegistry, NavigatorIOS } from "react-native";
 import SplashPage from "./ios/js/components/SplashPage";
 import { Provider } from "react-redux";
 import configureStore from "./ios/js/configureStore.js";
+import NativeTachyons from "react-native-style-tachyons";
+import { StyleSheet } from "react-native";
+
+NativeTachyons.build({
+  colors: {
+    palette: {
+      blue: "#1DACD6",
+      orange: "#FAC219",
+      white: "#FFFFFF",
+      black: "#000000",
+      red: "#FF0000"
+    }
+  }
+}, StyleSheet);
 
 const store = configureStore();
 
