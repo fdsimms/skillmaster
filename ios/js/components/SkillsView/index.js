525526733f0175f241ceb435d44c18224e0ca5a6
Frankie <fdsimms@gmail.com>
2016-09-25 20:01:22 -0400

    Move to SkillView after clicking a Skill

--- ios/js/components/SkillsView/index.js
+++ ios/js/components/SkillsView/index.js
@@ -3,34 +3,40 @@ import { connect } from "react-redux";
 import {
   Text,
   View,
-  ListView
+  ListView,
+  TouchableHighlight
 } from "react-native";
 
+import SkillView from "../SkillView";
 import { fetchSkills } from "../../apiUtils";
 import {
   showSpinner,
   hideSpinner,
+  changeScene,
   updateErrorMessage
 } from "../../actions";
 import { styles as s } from "react-native-style-tachyons";
 import { gs } from "../../styles/global";
 
-const Skill = ({ skill }) => (
+const Skill = ({ skill, onSelectSkill }) => (
   <View
       key={skill.id}
       style={[s.asc, s.jcc]}>
+    <TouchableHighlight onPress={() => onSelectSkill(skill)}>
+      <View style={[s["bg-orange"], s.h3, s.w3, s.br4, s.asc]} />
+    </TouchableHighlight>
     <Text style={[s.tc, s.darkGray, s.pb2]}>
       {skill.name}
     </Text>
   </View>
 );
 
-const Skills = ({ skills }) => {
+const Skills = ({ skills, onSelectSkill }) => {
   const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
   return Object.keys(skills).length > 0 ? (
     <ListView
       dataSource={ds.cloneWithRows(skills)}
-      renderRow={skill => <Skill skill={skill} />}
+      renderRow={skill => <Skill skill={skill} onSelectSkill={onSelectSkill} />}
     />
   ) : null;
 };
@@ -49,10 +55,19 @@ class SkillsView extends Component {
     }
   }
 
+  onSelectSkill(skill) {
+    this.props.dispatch(changeScene({
+      title: skill.name, component: <SkillView skillId={skill.id} />
+    }, this.props.navigator));
+  }
+
   render() {
     return (
       <View style={[gs.container, s.mt7]}>
-        <Skills skills={this.props.skills} />
+        <Skills
+          skills={this.props.skills}
+          onSelectSkill={this.onSelectSkill.bind(this)}
+        />
       </View>
     );
   }
